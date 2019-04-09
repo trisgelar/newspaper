45c5f8b7965c483fb79d5dfbd679bec0e9fa0590
Trisna Gelar <trisgelar@gmail.com>
2019-04-09 12:28:27 +0700

    Add Authorization and Comment to Articles

--- articles/views.py
+++ articles/views.py
@@ -1,28 +1,53 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Article
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin,CreateView):
 	model = Article
 	template_name = 'articles/new_view.html'
 	fields = ('title', 'body', 'author',)
+	login_url = 'login'
 
-class ArticleListView(ListView):
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+
+class ArticleListView(LoginRequiredMixin, ListView):
 	model = Article
 	template_name = 'articles/list_view.html'
+	login_url = 'login'
 
-class ArticleDetailView(DetailView):
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
 	model = Article
 	template_name = 'articles/detail_view.html'
+	login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
 	model = Article
 	fields = ('title', 'body')
 	template_name = 'articles/edit_view.html'
+	login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+	def dispatch(self, request, *args, **kwargs):
+		obj = self.get_object()
+		if obj.author != self.request.user:
+			raise PermissionDenied
+		return super().dispatch(request, *args, **kwargs)
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
 	model = Article
 	template_name = 'articles/delete_view.html'
 	success_url = reverse_lazy('article_list')
+	login_url = 'login'
+
+	def dispatch(self, request, *args, **kwargs):
+		obj = self.get_object()
+		if obj.author != self.request.user:
+			raise PermissionDenied
+		return super().dispatch(request, *args, **kwargs	)
